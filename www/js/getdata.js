ace7a9a522af8a1e872613d9afb82ffcb62a552b
fullmetal2000 <cheng.wangshu@gmail.com>
2013-06-06 23:41:58 -0700

    V0.5
    Support categoy feature.

---
+++ www/js/getdata.js
@@ -0,0 +1,32 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: wangshucheng
+ * Date: 13-05-12
+ * Time: 12:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var URLInfo;
+URLInfo = {
+baseurl:'',
+keywords:'',
+value:''
+    
+    
+};
+
+
+
+
+function goback(){
+    
+    
+    $('#page_'+localStorage.getItem('current_page_id')).find('.content').children().show();
+    
+    $('.detail_view').hide();
+    
+    
+}
+
+function gohome(){
+    $.mobile.changePage('#page_1');
+}
